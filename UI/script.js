7899af52422db01e4d97210a4b3f33ffb8d1b0cc
oOXpycTOo <vladkaz1999@gmail.com>
2019-03-05 18:00:28 +0300

    Addes css variables to control with and height

--- UI/script.js
+++ UI/script.js
@@ -1,14 +1,3 @@
-function resizeText() {
-    var elements  = document.getElementsByClassName('overlay-icons');
-    if (elements.length < 0) {
-        return;
-    }
-    for (var i = 0; i < elements.length; ++i) {
-        var el = elements[i];
-        console.log(el.scrollHeight);
-        el.style.fontSize = el.scrollHeight * 0.8 + "px";
-    }
-}
 
 document.querySelector('.menu-btn').addEventListener('click', function() {
     this.classList.toggle('active');
