2424e1838db709ab71e67ed982bbd93da5384bda
oOXpycTOo <vladkaz1999@gmail.com>
2019-05-26 06:48:00 +0300

    Added Lab 8 and Lab 11

---
+++ ImaginePhotos/src/main/java/app/servlets/Test1Servlet.java
@@ -0,0 +1,15 @@
+package app.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Test1Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("Views/status.jsp").forward(request, response);
+    }
+}
---
+++ ImaginePhotos/web/Views/status.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vladk
+  Date: 25.05.2019
+  Time: 10:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>ImaginePhotos</title>
+</head>
+<body>
+    <p style="color: red;">Application is running</p>
+</body>
+</html>
